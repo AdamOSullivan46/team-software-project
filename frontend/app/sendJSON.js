14cb9700ad9f6130f1a6c93b1906aef9b041d53f
douglaslordan <35471251+douglaslordan@users.noreply.github.com>
2018-01-24 13:03:51 +0000

    Created a seperate die roll and game start request

--- frontend/app/sendJSON.js
+++ frontend/app/sendJSON.js
@@ -3,6 +3,11 @@ export function generateGameStartJSON() {
     return JSON.stringify({type: 'gameStart'});
 }
 
+// Generate JSON to send to server on game start.
+export function generateDieRollJSON() {
+    return JSON.stringify({type: 'dieRoll', name: document.getElementByID("username").value;});
+}
+
 // Send a POST request to the server for the game start.
 export function gameStartRequest(serverAddress, callback) {
     const ajaxRequest = new XMLHttpRequest();
@@ -10,4 +15,13 @@ export function gameStartRequest(serverAddress, callback) {
     ajaxRequest.onreadystatechange = () => callback(ajaxRequest);
     ajaxRequest.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     ajaxRequest.send(generateGameStartJSON());
+}
+
+// Send a POST request to the server for a die roll.
+export function dieRollRequest(serverAddress, callback) {
+    const ajaxRequest = new XMLHttpRequest();
+    ajaxRequest.open('POST', serverAddress, true);
+    ajaxRequest.onreadystatechange = () => callback(ajaxRequest);
+    ajaxRequest.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    ajaxRequest.send(generateDieRollJSON());
 }
