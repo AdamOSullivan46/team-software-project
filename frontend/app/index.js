b4b9cdada23922bdb074d0c21c19689d3bbf6c88
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-30 21:34:48 +0000

    Use browser example code for websockets

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -58,4 +58,15 @@ window.onload = () => {
             callback: () => {}
         });
     };
+
+    let ws = new WebSocket("ws://ws2:5678/"),
+        messages = document.createElement('ul');
+    ws.onmessage = function (event) {
+        var messages = document.getElementsByTagName('ul')[0],
+            message = document.createElement('li'),
+            content = document.createTextNode(event.data);
+        message.appendChild(content);
+        messages.appendChild(message);
+    };
+    document.body.appendChild(messages);
 };
