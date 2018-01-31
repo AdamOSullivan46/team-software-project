91c9a8745c8e24a8f594322f22b384f4a7903a6d
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-31 10:01:42 +0000

    Reindent server code and import protocol

--- backend/backend/websocket.py
+++ backend/backend/websocket.py
@@ -1,23 +1,24 @@
 if __name__ == '__main__':
 
-   try:
-      import asyncio
-   except ImportError:
-      ## Trollius >= 0.3 was renamed
-      import trollius as asyncio
+    try:
+        import asyncio
+    except ImportError:
+        ## Trollius >= 0.3 was renamed
+        import trollius as asyncio
+    from websocket_protocol import MyServerProtocol
 
-   from autobahn.asyncio.websocket import WebSocketServerFactory
-   factory = WebSocketServerFactory()
-   factory.protocol = MyServerProtocol
+    from autobahn.asyncio.websocket import WebSocketServerFactory
+    factory = WebSocketServerFactory()
+    factory.protocol = MyServerProtocol
 
-   loop = asyncio.get_event_loop()
-   coro = loop.create_server(factory, 'localhost', 9000)
-   server = loop.run_until_complete(coro)
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(factory, 'localhost', 9000)
+    server = loop.run_until_complete(coro)
 
-   try:
-      loop.run_forever()
-   except KeyboardInterrupt:
-      pass
-   finally:
-      server.close()
-      loop.close()
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.close()
