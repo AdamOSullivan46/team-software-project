c6990326fcecfd99297375896895bdf128ab6a71
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-31 09:42:52 +0000

    Replace websocket client code with echo server
    This was supposed to be a server, but I got my files mixed up.

--- backend/backend/websocket.py
+++ backend/backend/websocket.py
@@ -1,9 +1,10 @@
 import websockets
 import asyncio
 
-async def hello(uri):
-    async with websockets.connect(uri) as websocket:
-        await websocket.send("Hello world!")
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send(message)
 
 asyncio.get_event_loop().run_until_complete(
-    hello('ws://localhost:8765'))
+    websockets.serve(echo, 'localhost', 8765))
+asyncio.get_event_loop().run_forever()
