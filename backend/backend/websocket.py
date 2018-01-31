38fa6344fe8edda910fecccd77c8d9b4e17d7a95
Noel Bourke <undergroundquizscene@gmail.com>
2018-01-31 09:53:18 +0000

    Copy Autobahn example echo server (using asyncio)
    I want to try another websocket implementation and see if the problem is
    with the one I was using.

--- backend/backend/websocket.py
+++ backend/backend/websocket.py
@@ -1,10 +1,23 @@
-import websockets
-import asyncio
+if __name__ == '__main__':
 
-async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
+   try:
+      import asyncio
+   except ImportError:
+      ## Trollius >= 0.3 was renamed
+      import trollius as asyncio
 
-asyncio.get_event_loop().run_until_complete(
-    websockets.serve(echo, 'localhost', 8765))
-asyncio.get_event_loop().run_forever()
+   from autobahn.asyncio.websocket import WebSocketServerFactory
+   factory = WebSocketServerFactory()
+   factory.protocol = MyServerProtocol
+
+   loop = asyncio.get_event_loop()
+   coro = loop.create_server(factory, 'localhost', 9000)
+   server = loop.run_until_complete(coro)
+
+   try:
+      loop.run_forever()
+   except KeyboardInterrupt:
+      pass
+   finally:
+      server.close()
+      loop.close()
